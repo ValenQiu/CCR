f9a888b6422dbce03a77d499f6dcf792b128df51
ValenQ <valen.qiu@connect.polyu.hk>
2023-02-28 08:49:17 +0100

    can use now

--- Aruco_PoseEstimation/arucoPoseEstimation.py
+++ Aruco_PoseEstimation/arucoPoseEstimation.py
@@ -5,9 +5,9 @@ import cv2.aruco as aruco
 import math
 
 # Base ID:
-base_id = 0
+base_id = 99
 # Track ID:
-track_id = 1
+track_id = 11
 
 # Camera
 fx = 1.07588078e+03
@@ -24,12 +24,67 @@ cameraMatrix = np.array([[fx, 0, cx],
                          [0, 0, 1]])
 dist = np.array([k1, k2, p1, p2, k3])
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
+
+relativervec, relativetvec = None, None
 
 
-def DetectArucoPose():
+def cal_Points(ids, corners, rvec, tvec):
+    # return three arrays: center, rvecs, tvecs
+    # the first components in each arrays are the pose of the base marker
+    # the second components in each arrays are the pose of the track marker
+    if ids is not None:
+        center = np.full([2, 2], None)
+        rvecs = np.full(2, None)
+        tvecs = [[0, 0, 0], [0, 0, 0]]
+        for i in range(len(ids)):
+            if ids[i] == base_id:
+                corner_base = corners[i][0]
+                center_x = (corner_base[0][0] + corner_base[2][0]) / 2
+                center_y = (corner_base[0][1] + corner_base[2][1]) / 2
+                center[0][0] = center_x
+                center[0][1] = center_y
+                rvecs[0] = rvec[i][0]
+                tvecs[0] = tvec[i][0]
+            elif ids[i] == track_id:
+                corner_track = corners[i][0]
+                center_x = (corner_track[0][0] + corner_track[2][0]) / 2
+                center_y = (corner_track[0][1] + corner_track[2][1]) / 2
+                center[1][0] = center_x
+                center[1][1] = center_y
+                rvecs[1] = rvec[i][0]
+                tvecs[1] = tvec[i][0]
+        if not np.any(center == None):
+            center = np.int0(center)
+        return center, rvecs, tvecs
+
+
+def inversePerspective(rvec, tvec):
+    R, _ = cv2.Rodrigues(rvec)
+    R = np.matrix(R).T
+    invTvec = np.dot(R, np.matrix(-tvec))
+    invRvec, _ = cv2.Rodrigues(R)
+    return invRvec, invTvec
+
+
+def relativePosion(rvec1, tvec1, rvec2, tvec2):
+    rvec1, tvec1 = rvec1.reshape((3, 1)), tvec1.reshape((3, 1))
+    rvec2, tvec2 = rvec2.reshape((3, 1)), tvec2.reshape((3, 1))
+    # Inverse the second marker
+    invRvec, invTvec = inversePerspective(rvec2, tvec2)
+    info = cv2.composeRT(rvec1, tvec1, invRvec, invTvec)
+    composedRvec, composedTvec = info[0], info[1]
+    composedRvec = composedRvec.reshape((3, 1))
+    composedTvec = composedTvec.reshape((3, 1))
+    return composedRvec, composedTvec
+
+
+def arucoIdentify():
+    global relativervec, relativetvec
     ret, frame = camera.read()
     color_image = cv2.resize(frame, (640, 480))
+    # cv2.imshow("frame", frame)
+
     h1, w1 = color_image.shape[:2]
     newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (h1, w1), 0, (h1, w1))
     frame = cv2.undistort(color_image, cameraMatrix, dist, None, newCameraMatrix)
@@ -46,11 +101,24 @@ def DetectArucoPose():
             aruco.drawAxis(frame, cameraMatrix, dist, rvec[i, :, :], tvec[i, :, :], 0.03)
             aruco.drawDetectedMarkers(frame, corners, ids)
             # print("rvec[", i, ",: , ：]: ", rvec[i, :, :])
-    cv2.imshow("arucoDetector", frame)
+        cv2.imshow("arucoDetector", frame)
+        centers, rotationVec, transferVec = cal_Points(ids, corners, rvec, tvec)
+        # print("centers\n", centers)
+        # print("rotational Vectors\n", rotationVec)
+        # print("transfer Vector", transferVec)
+        if all(rotationalVector is not None for rotationalVector in rotationVec):
+            relativervec, relativetvec = relativePosion(rotationVec[0], transferVec[0], rotationVec[1], rotationVec[1])
+            # print("relativervec\n", relativervec)
+            # print("relativetvec\n", relativetvec)
+            # print("type of relativervec: ", type(rotationVec))
+
 
 
 while True:
     key = cv2.waitKey(1)
     if key & 0xFF == ord('q') or key == 27:
         break
-    DetectArucoPose()
+    arucoIdentify()
+    print("relativervec\n", relativervec)
+    print("relativetvec\n", relativetvec)
+    # arucoIdentify()
